832358164f636df0d9e7e74536c8f5cac4673a70
Dhanya@1306 <thulasi150595@gmail.com>
2025-05-20 22:56:53 +0530

    checking merge

--- src/emp/LsitMap.java
+++ src/emp/LsitMap.java
@@ -32,23 +32,10 @@ public class LsitMap {
 
 */
 		
-		Map<String,Integer> m=new LinkedHashMap<String,Integer>();
-		m.put("Java",100);
-		m.put("Selenium", 200);
-		Map<String,Integer> m1=new LinkedHashMap<String,Integer>();
-		m1.put("developer", 300);
-		m1.put("testing", 400);
 		
-		Integer i=m.get("Java");
-		System.out.println(i);
-		
-		List<Map<String,Integer>> l=new LinkedList<Map<String,Integer>>();
-		l.add(m);
-		l.add(m1);
-		Map<String,Integer> i1=l.get(1);
-		Integer mm=i1.get("testing");
-		System.out.println(mm);
-		
+	private static void statement1() {
+		// TODO Auto-generated method stub
 		
 	}
 }
+
